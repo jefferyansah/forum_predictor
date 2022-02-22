ac7384bae552211a7c11688b5449513b28830d58
Jeff <vanslerry@gmail.com>
2022-02-23 09:13:12 +1000

    Clean up UI

--- frontend/main.py
+++ frontend/main.py
@@ -3,18 +3,26 @@ import requests
 
 
 
-st.header('Welcome to the Forum Predictor App built')
+st.header('Forum Predictor App')
+st.image('eliza.png',width=100)
 st.write('Type a question, and I can predict which forum that questions is coming from')
+st.write('Questions should come from these categories: `quantumcomputing`, `astronomy`, `martialarts`, `opendata`,`sports`, `ai`, `computergraphics`, `coffee`, `beer`')
 question = st.text_input('Enter Text')
 send = st.button('Predict Forums')
 keys= {'question': question}
 
 
+if send and (question.strip() == '' or len(question.strip()) < 10):
+    st.warning('Please enter an actual question')
 
 
-if send:
-    prediction = requests.get("http://137.184.226.129/predict-forum", params=keys)
-    prediction = prediction.json()
-    st.write(prediction)
+elif send and question != '':
+    with st.spinner('Wait for it...'):
+        prediction = requests.get("http://137.184.226.129/predict-forum", params=keys)
+        prediction = prediction.json()
+        st.write('Predicted Class: `{}`'.format(prediction['prediction']))
+        st.write('Probability: `{}`'.format(prediction['Probability']))
+        st.success('Done!')
+
 
 
